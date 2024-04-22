f7e88ee4d30ceb2c5df6a79cba852cb2f12f9dd0
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2024-04-22 18:40:07 +0200

    improve jjstlog.sh

--- scripts/jjstlog.sh
+++ scripts/jjstlog.sh
@@ -1,2 +1,23 @@
 #! /usr/bin/zsh
-{jj st --color always | grep -vE "(Working|Parent)"} && echo "\n========\n" && jj log --color always
+
+header() {
+  separator
+  #echo -n "# "
+  echo $1
+  separator
+}
+
+separator() {
+  local sep=${1:-=}
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' $sep
+}
+
+get_cwd() {
+    local cwd="${PWD/#$HOME/~}"  # Replace $HOME with ~
+    echo "$cwd"
+}
+
+#get_cwd
+jj st --color always | grep -vE "(Working|Parent)" 
+echo
+jj log --color always
